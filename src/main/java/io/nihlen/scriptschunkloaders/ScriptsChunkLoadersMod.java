a639a9a484ac07cf9fe6d7b7abefed87f8266117
Henning Raudzis <121398546+Craemon@users.noreply.github.com>
2026-04-04 23:18:27 +0200

    chore: add TicketType CUSTOM_TICKETTYPE_FORCED

--- src/main/java/io/nihlen/scriptschunkloaders/ScriptsChunkLoadersMod.java
+++ src/main/java/io/nihlen/scriptschunkloaders/ScriptsChunkLoadersMod.java
@@ -2,6 +2,9 @@ package io.nihlen.scriptschunkloaders;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.server.level.TicketType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +16,8 @@ public class ScriptsChunkLoadersMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 	public static final ChunkLoaderManager CHUNK_LOADER_MANAGER = new ChunkLoaderManager();
+	// Add Custom TicketType, Flags are set to mimic FORCED.
+	public static final TicketType CUSTOM_TICKETTYPE_FORCED =  Registry.register(BuiltInRegistries.TICKET_TYPE, ScriptsChunkLoadersMod.MODID, new TicketType(TicketType.NO_TIMEOUT, TicketType.FLAG_PERSIST | TicketType.FLAG_LOADING | TicketType.FLAG_SIMULATION | TicketType.FLAG_KEEP_DIMENSION_ACTIVE));
 
 	@Override
 	public void onInitialize() {
